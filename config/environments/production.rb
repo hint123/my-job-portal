21f03800774b47166a7b08035160c6c4b6281548
kamlesh <ciskamlesh@gmail.com>
2013-10-06 13:05:06 +0000

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,9 @@ JobPortal::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+  config.assets.initialize_on_precompile = false
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
